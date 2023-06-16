113a28ed40e9cb79f38030f8f66b3345ba16a4e4
dowon kim <32028454+TheON2@users.noreply.github.com>
2023-06-16 09:43:00 +0900

    [level 1] Title: 직사각형 별찍기, Time: 45.14 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -1,13 +1,10 @@
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
-  let n = data.split(" ");
-  const a = Number(n[0]), b = Number(n[1]);
-  n = '';
-  for (let i = 0; i < b ; i++) {
-    for (let j = 0; j < a ; j++) {
-      n+='*'
+    const n = data.split(" ");
+    const a = Number(n[0]), b = Number(n[1]);
+    const row = '*'.repeat(a)
+    for(let i =0; i < b; i++){
+        console.log(row)
     }
-    n+='\n'
-  }
-    console.log(n)
+
 });
