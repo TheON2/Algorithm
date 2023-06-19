888b89852a94120b76be7002c039367f20a5488d
dowon kim <32028454+TheON2@users.noreply.github.com>
2023-06-19 20:10:42 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.58 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,4 +1,8 @@
 function solution(numbers) {
+    //중복 값을 받지 않는 Set 인스턴스 선언
+    //인덱스가 겹치지 않을때만 두수의 합을 add한다
+    //구조분해할당으로 Set값들을 꺼낸 배열을 만들고
+    //정렬하여 반환
   let ans = new Set();
   for (let i = 0; i < numbers.length; i++) {
     for (let j = 0; j < numbers.length; j++) {
