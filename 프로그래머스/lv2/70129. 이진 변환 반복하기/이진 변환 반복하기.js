4634a4750f15e064361f4775cefe0af3b623fec8
dowon kim <32028454+TheON2@users.noreply.github.com>
2023-07-25 08:31:13 +0900

    Update 이진 변환 반복하기.js

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,13 +1,18 @@
 function solution(s) {
   let transformCount = 0;
+  //변환 시도 횟수
   let removedZeroCount = 0;
-
+  //제거한 0의 갯수
   while (s !== "1") {
     let zeroCount = (s.match(/0/g) || []).length;
     s = s.replace(/0/g, '');
+    //정규식으로 0의 갯수를 찾아낸후 0의 요소를 배열에서 제거한다
     removedZeroCount += zeroCount;
+    //제거한 숫자만큼 카운팅
     s = s.length.toString(2);
+    //2진법으로 문자열을 변환
     transformCount++;
+    //변환 횟수 카운팅
   }
 
   return [transformCount, removedZeroCount];
