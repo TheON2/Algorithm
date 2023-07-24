05479fdff9db139656764992b726f3448ab43747
dowon kim <32028454+TheON2@users.noreply.github.com>
2023-07-25 08:42:31 +0900

    Update 최댓값과 최솟값.js

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,4 +1,6 @@
 function solution(s) {
     const numbers = s.split(' ').map(Number);
+    //문자열을 공백기준으로 자른 배열의 요소를 map으로 돌며 숫자로 형변환한다.
     return Math.min(...numbers) + ' ' + Math.max(...numbers);
+    //구조분해할당으로 numbers의 모든 요소의 최솟값과 최댓값을 구해 공백을 기준으로 붙여 반환
 }
