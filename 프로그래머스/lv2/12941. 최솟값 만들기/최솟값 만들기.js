3d2dc00f38447e99acd20782123c1142a0e88b42
dowon kim <32028454+TheON2@users.noreply.github.com>
2023-07-25 08:48:06 +0900

    Update 최솟값 만들기.js

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,6 +1,10 @@
 function solution(A,B){
+  //그리디 알고리즘을 활용한다.
+  //각 항목에서 최대한 큰값과 작은값을 매칭하여 곱해주는 경로를 찾는다.
+  
   A.sort((a, b) => a - b);
   B.sort((a, b) => b - a);
+  //한쪽은 오름차순 한쪽은 내림차순
 
   let answer = 0;
   for(let i=0; i<A.length; i++) {
